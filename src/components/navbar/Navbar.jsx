a482167adbbda83eb552001bd73e93969315ff5b
Flu3nt <gnedyalkov94@gmail.com>
2022-11-09 13:54:34 +0200

    Add email icon to navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,4 +1,5 @@
 import "./Navbar.css"
+import EmailIcon from "@mui/icons-material/Email"
 
 const Navbar = () => {
   return (
@@ -10,7 +11,11 @@ const Navbar = () => {
         <li className="nav-link">about</li>
         <li className="nav-link">contact</li>
       </ul>
-      <span className="nav-contact"></span>
+      <span className="nav-contact">
+        <a href="mailto: gnedyalkov94@gmail.com" className="nav-email">
+          <EmailIcon className="nav-icon" />
+        </a>
+      </span>
     </div>
   )
 }
---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import "./Home.css"
+import Navbar from "../components/navbar/Navbar"
+
+const Home = () => {
+  return (
+    <div className="home">
+      <Navbar />
+    </div>
+  )
+}
+
+export default Home
